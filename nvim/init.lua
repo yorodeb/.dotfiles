475bca1d54cf4a097f991666146f65f4e070b0b1
Kshitij Yadav <kshitijyadav651@gmail.com>
2026-01-06 16:29:38 +0000

    Auto Complete

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,6 @@
--- Editor Configuration
+-- ========================================================================== --
+--                          EDITOR CONFIGURATION                              --
+-- ========================================================================== --
 local option = vim.opt
 
 option.number = true
@@ -17,8 +19,14 @@ option.splitright = true
 option.clipboard = "unnamedplus"
 option.termguicolors = true
 
--- Plugin Manager
+-- VS Code-like completion behavior
+-- menuone: popup even if there's only one match
+-- noselect: don't force-select the first item (let you keep typing)
+option.completeopt = { "menu", "menuone", "noselect" }
 
+-- ========================================================================== --
+--                          PLUGIN MANAGER (lazy.nvim)                         --
+-- ========================================================================== --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -35,16 +43,46 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- ========================================================================== --
+--                               PLUGINS                                      --
+-- ========================================================================== --
 require("lazy").setup({
   spec = {
-    {'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true},
-		{"bluz71/vim-moonfly-colors", lazy = false, priority = 1000, 
-    config = function()
-      vim.cmd([[colorscheme moonfly]])
-    end,
-  },
+    -- 1. Autopairs: Auto-close brackets () [] {}
+    {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      config = true
+    },
+
+    -- 2. Mini.Completion: Lightweight VS Code-like autocomplete
+    {
+      'echasnovski/mini.completion',
+      version = false,
+      config = function()
+        require('mini.completion').setup({
+          window = {
+            completion = { border = 'rounded', winblend = 10 },
+            info = { border = 'rounded', winblend = 10 },
+          },
+          -- 50ms delay for a snappy VS Code feel
+          delay = { completion = 50, info = 50 },
+        })
+
+        -- Keybindings for VS Code behavior
+        local imap_expr = function(lhs, rhs)
+          vim.keymap.set('i', lhs, rhs, { expr = true, replace_keycodes = false })
+        end
+
+        -- Use Tab to go down, Shift-Tab to go up
+        imap_expr('<Tab>',   [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+        imap_expr('<S-Tab>', [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+        
+        -- Use Enter to confirm the selection (and not create a new line)
+        imap_expr('<CR>',    [[pumvisible() ? "\<C-y>" : "\<CR>"]])
+      end
+    },
+
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
